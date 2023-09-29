805699e5676638cde33b2b3ea71c3fd080dab54f
Aadil <cb010188@students.apiit.lk>
2023-09-29 13:54:46 +0530

    initial cmmit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+
+
+export default function App() {
+  
+  const [todos, setTodos] = useState([""])
+  
+function addTodo(title) {
+       setTodos((currentTodos) => {
+          return [
+            ...currentTodos,
+            { id: crypto.randomUUID(), title, completed:
+               false },
+          ]
+        })
+    
+}
+  
+function toggleTodo (id, completed) {
+  setTodos((currentTodos) => {
+    return currentTodos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed }
+      }
+      return todo
+    })
+  })
+}
+
+function deleteTodo (id){
+  setTodos((currentTodos) => {
+    return currentTodos.filter((todo) => todo.id !== id)
+  } )
+}
+
+  return (
+  <>
+<NewTodoForm onSubmit={addTodo} />
+
+<h3 className="Header">Todo List</h3>
+<ul className="list">
+{todos.map(todo => {
+  return (
+    <li key={todo.id}>
+      <label>
+        <input type="checkbox" checked={todo.completed} 
+        onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+        {todo.title}
+      </label>
+      <button onClick={() => deleteTodo(todo.id)} className="btn btn danger">delete</button>
+    </li>
+  );
+})}
+</ul>
+
+</>
+)
+}
+
